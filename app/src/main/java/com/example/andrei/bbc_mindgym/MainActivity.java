1a38e3b8817f332358018e5fb0c33e93a106114c
Daci <dacianflorea99@gmail.com>
2019-03-23 22:11:10 +0200

    Add workout button

--- app/src/main/java/com/example/andrei/bbc_mindgym/MainActivity.java
+++ app/src/main/java/com/example/andrei/bbc_mindgym/MainActivity.java
@@ -88,10 +88,15 @@ public class MainActivity extends AppCompatActivity
 
         } else if (id == R.id.nav_trackedapps) {
 
+        } else if (id == R.id.nav_workouts){
+
         }
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         drawer.closeDrawer(GravityCompat.START);
         return true;
+    }
+    public void openWorkoutsTab(){
+        
     }
 }
