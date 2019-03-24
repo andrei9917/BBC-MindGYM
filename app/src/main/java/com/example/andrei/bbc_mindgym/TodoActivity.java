1d1f5cc0c012aa7bf8422874e1aee2372449519f
andrei9917 <tanca_andrei@yahoo.com>
2019-03-24 02:52:38 +0200

    Icon changes v2

--- app/src/main/java/com/example/andrei/bbc_mindgym/TodoActivity.java
+++ app/src/main/java/com/example/andrei/bbc_mindgym/TodoActivity.java
@@ -1,13 +1,48 @@
 package com.example.andrei.bbc_mindgym;
 
 import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.TextInputLayout;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class TodoActivity extends AppCompatActivity {
+
+    ListView lv;
+    FloatingActionButton button;
+    TextInputLayout text;
+    List<String> itemlist;
+    ArrayAdapter<String> arrayAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_todo);
+
+        lv = findViewById(R.id.listView);
+        button = findViewById(R.id.floatingActionButton);
+        text = findViewById(R.id.textInputLayout);
+        itemlist = new ArrayList<>();
+        arrayAdapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, itemlist);
+        lv.setAdapter(arrayAdapter);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                addToList();
+            }
+        });
+    }
+
+    public void addToList(){
+        if(!text.getEditText().getText().toString().equals("")){
+            String toDoStr = text.getEditText().getText().toString();
+            itemlist.add(toDoStr);
+            arrayAdapter.notifyDataSetChanged();}
     }
 }
